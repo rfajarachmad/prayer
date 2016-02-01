b14f714c1c282d186c68f65370c1b386fe8cc4b2
rfajarachmad <achmadfajar.ridwan@mitrais.com>
2016-02-02 00:00:06 +0700

    Show/hide icon based on notification settings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -165,7 +165,7 @@ angular.module('starter.controllers', [])
         prayer.praytime.isha.notification = $scope.notification;
         break;
     }
-
+    $scope.prayer = prayer;
     Configuration.setPrayer(prayer); 
     $log.debug("Saving prayer", prayer);
   }
@@ -351,25 +351,6 @@ angular.module('starter.controllers', [])
   $scope.prayer = prayer;
   $scope.query = query;
 
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-$scope.chats = Chats.all();
-$scope.remove = function(chat) {
-  Chats.remove(chat);
-};
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('SettingsCtrl', function($scope, $rootScope, $log, Configuration, Lookup) {
